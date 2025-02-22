fc9db7d7a0400be9aabe211af2b4cc7c190e358c
JaViKo500 <javikogutierrez64@gmail.com>
2025-02-22 12:45:27 -0500

    validation dni

--- examples/index.ts
+++ examples/index.ts
@@ -1,4 +1,16 @@
 import { dniValidation } from '../src';
 
-const dni = '0105566046';
-dniValidation(dni);
+const listDni = [
+  '0105566046',
+  '12345678A1',
+  '2505566046',
+  '01A5566046',
+  '0185566046',
+  '0105566047',
+  '0105566038',
+];
+
+listDni.forEach((dni) => {
+  const isValid = dniValidation(dni);
+  console.log(isValid);
+});
--- src/validations/dni-validation.ts
+++ src/validations/dni-validation.ts
@@ -1,4 +1,20 @@
-export const dniValidation = (dni: string) => {
-  console.log('<--------------- JK Dni-validation --------------->');
-  console.log(dni);
+import { algorithm10, initValidate, validateCodeProvince, validateThirdDigit } from '../helpers';
+import { Result, TypeIdentification } from '../interfaces';
+
+
+export const dniValidation = (dni: string): Result => {
+  try {
+    initValidate( dni, TypeIdentification.DNI );
+    validateCodeProvince(dni.substring(0, 2));
+    validateThirdDigit(dni.substring(2, 3), TypeIdentification.DNI);
+    algorithm10(dni.substring(0, 9), dni.substring(9, 10));
+  } catch (error: any) {
+    return {
+      isValid: false,
+      errorMessage: error.message,
+    }
+  }
+  return {
+    isValid: true,
+  };
 }
